f655c3ee238bc754defb5309679e68e897ffd438
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2019-03-09 14:33:28 -0500

    Update TopologicalSort.java

--- TopologicalSort.java
+++ TopologicalSort.java
@@ -8,18 +8,16 @@ public class TopologicalSort {
         this.reversePostOrder = new Stack <>();
         this.marked = new boolean[graph.getVertices()];
         for (int vertex = 0; vertex < graph.getVertices(); vertex++) {
-            if (!marked[vertex])
-                dfs(graph,vertex);
+            if (!marked[vertex]) dfs(graph,vertex);
         }
 
     }
 
     private void dfs(DiGraph graph, int vertex) {
         this.marked[vertex] = true;
-        for (int w : graph.adj(vertex)) {
-            if (!marked[w]) dfs(graph,w);
-            this.reversePostOrder.push(vertex);
-        }
+        for (int w : graph.adj(vertex)) if (!marked[w]) dfs(graph,w);
+        this.reversePostOrder.push(vertex);
+
     }
 
     public TopologicalSort(EdgeWeightedDiGraph graph) {
@@ -34,13 +32,26 @@ public class TopologicalSort {
 
     private void dfs(EdgeWeightedDiGraph graph, int vertex) {
         this.marked[vertex] = true;
-        for (DirectedEdge e : graph.adj(vertex)) {
-            if (!marked[e.to()]) dfs(graph,e.to());
-            this.reversePostOrder.push(vertex);
-        }
+        for (DirectedEdge e : graph.adj(vertex)) if (!marked[e.to()]) dfs(graph,e.to());
+        this.reversePostOrder.push(vertex);
     }
-    
     public Iterable<Integer> reversePost() {
         return this.reversePostOrder;
+    }
+
+    public static void main(String[] args) {
+        final int vertices = 7;
+        DiGraph graph = new DiGraph(vertices);
+        System.out.println(graph.getVertices());
+        System.out.println(graph.getEdges());
+        System.out.println("**********");
+        graph.addEdge(0,1); graph.addEdge(0,5);
+        graph.addEdge(0,2); graph.addEdge(1,4);
+        graph.addEdge(3,2); graph.addEdge(3,5);
+        graph.addEdge(3,4); graph.addEdge(3,6);
+        graph.addEdge(5,2); graph.addEdge(6,0);
+        graph.addEdge(6,4);
+        TopologicalSort topologicalSort = new TopologicalSort(graph);
+        for (int v : topologicalSort.reversePostOrder) System.out.println(v);
     }
 }
