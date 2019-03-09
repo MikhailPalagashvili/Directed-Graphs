025510e93f9489640efbffc71326d33c5f82f46d
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2019-03-09 14:22:27 -0500

    Update DiGraph.java

--- DiGraph.java
+++ DiGraph.java
@@ -26,6 +26,10 @@ public class DiGraph {
         vertices++;
     }
 
+    public int getEdges() {
+        return this.adj.size();
+    }
+
     public DiGraph reverse() {
         DiGraph reverse = new DiGraph(this.vertices);
         for (int v = 0; v < reverse.getVertices(); v++) {
@@ -37,5 +41,24 @@ public class DiGraph {
 
     public int getVertices() {
         return this.vertices;
+    }
+
+    public static void main(String[] args) {
+        final int vertices = 13;
+        DiGraph graph = new DiGraph(vertices);
+        System.out.println(graph.getVertices());
+        System.out.println(graph.getEdges());
+        graph.addEdge(0,5); graph.addEdge(0,1);
+        graph.addEdge(2,0); graph.addEdge(6,0);
+        graph.addEdge(5,4); graph.addEdge(3,5);
+        graph.addEdge(4,3); graph.addEdge(4,2);
+        graph.addEdge(2,3); graph.addEdge(3,2);
+        graph.addEdge(11,4); graph.addEdge(6,4);
+        graph.addEdge(6,9); graph.addEdge(7,6);
+        graph.addEdge(6,8); graph.addEdge(8,6);
+        graph.addEdge(7,9); graph.addEdge(9,10);
+        graph.addEdge(12,9); graph.addEdge(11,12);
+        graph.addEdge(9,11);
+        for (int v = 0; v < graph.getVertices(); v++) for (int w : graph.adj(v)) System.out.println(v + " -> " + w);
     }
 }
